b58a405d1e6156c1a242f07ea7b47a78e6ea8336
a84059 <franciscocunha888@gmail.com>
2021-04-14 15:05:21 +0000

    Modelos3d

---
+++ assets/js/modelos3d.js
@@ -0,0 +1,36 @@
+const urlBase = "https://ptsibackend.herokuapp.com/modelos"
+
+window.onload = () => {
+    // References to HTML objects   
+    const tblmodelos = document.getElementById("tblmodelos")
+
+    const rendermodelos3d = async() => {
+        let strHtml = `
+          <thead>
+           <tr>
+           <td>Ficheiro</td>
+           <td>Descrição</td>
+           <td>Inventário</td>
+           <td>Autor</td>
+           </tr>
+          </thead>
+        `
+        const response = await fetch(`${urlBase}`)
+        const modelos = await response.json()
+        for (const modelo of modelos) {
+            strHtml += `
+                <tr>
+                    <td><iframe title="Termas Bracara Fase IV" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="fullscreen; autoplay; vr" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="${modelo.ficheiro}"></iframe></td>
+                     <td>${modelo.descricao}</td>
+                     <td>${modelo.n_inventario}</td>
+                     <td>${modelo.autor}</td>
+                </tr>
+                
+            `
+        }
+        strHtml += "</tbody>"
+        tblmodelos.innerHTML = strHtml
+    }
+
+    rendermodelos3d()
+}
