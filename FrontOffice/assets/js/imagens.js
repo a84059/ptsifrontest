e57b00f01c5f485f0a8f95211bcdc1651e04ae0c
a84059 <franciscocunha888@gmail.com>
2021-05-21 10:25:39 +0000

    Navbar

--- FrontOffice/assets/js/imagens.js
+++ FrontOffice/assets/js/imagens.js
@@ -33,7 +33,7 @@ window.onload = () => {
         lightbox.id = 'lightbox'
         document.body.appendChild(lightbox)
 
-    const images = document.querySelectorAll('img')
+        const images = document.querySelectorAll('img')
         images.forEach(image => {
             image.addEventListener('click', e => {
                 lightbox.classList.add('active')
